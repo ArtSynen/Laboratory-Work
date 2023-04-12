592670fc7269e870f8042518445a020d238d92f0
ArtSynen <117457828+ArtSynen@users.noreply.github.com>
2023-04-12 20:52:56 +0300

    Add files via upload

---
+++ LAB4/LAB4.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int val;
+	float fdisc = 0.05, sdisc = 0.07;
+	cout << "Input value of product ";
+	cin >> val;
+	if (val > 50 && val < 100)
+	{
+		cout << "Pay with discount = " << val - (val * fdisc) << " uah";
+	}
+	else if (val > 100)
+	{
+		cout << "Pay with discount = " << val - (val * sdisc) << " uah";
+	}
+	else
+	{
+		cout << "Pay = " << val << " uah";
+	}
+}
