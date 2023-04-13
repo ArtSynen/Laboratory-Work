1e4324792808ac49606ef74f1e1da1116682c881
ArtSynen <117457828+ArtSynen@users.noreply.github.com>
2023-04-13 12:55:04 +0300

    Add files via upload

---
+++ LAB7/LAB7.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+using namespace std;
+
+int n, l, mMax, mMin;
+
+void MaxMin(int n, int l)
+{
+    int array[10][10];
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < l; j++)
+        {
+            cout << "Enter number in array[" << i << "][" << j << "] = ";
+            cin >> array[i][j];
+        }
+    }
+    mMin = array[0][0];
+    mMax = array[0][0];
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < l; j++)
+        {
+            if (array[i][j] < mMin)
+            {
+                mMin = array[i][j];
+            }
+        }
+    }
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < l; j++)
+        {
+            if (array[i][j] > mMax)
+            {
+                mMax = array[i][j];
+            }
+        }
+    }
+}
+int main()
+{
+    cout << "Enter number of row = ";
+    cin >> n;
+    cout << "Enter number of column = ";
+    cin >> l;
+    MaxMin(n, l);
+    cout << "Min number of array = " << mMin << "\n";
+    cout << "Max number of array = " << mMax << "\n";
+}
